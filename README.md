675b3202a37ef31b32a634efca1fdc3dfd649eb7
Meron Brouwer <meron.brouwer@gmail.com>
2017-05-17 13:14:17 +0200

    Server is now based on express.js

---
+++ README.md
@@ -0,0 +1,8 @@
+# GurpsDomainMockServer 
+
+This is a very simple node.js application that can serve as a mock 
+for gurpsDomain. It will listen for a post on /convert and will respond 
+with a json output sheet.
+
+# Run
+node index.js
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-// content of index.js
-const http = require('http')
+const express = require('express')
+const app = express()
 const port = 3000
 
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
-}
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-const server = http.createServer(requestHandler)
+app.get('/', (request, response) => {
+    response.send('Hello from GurpsDomainMockServer!')
+})
 
-server.listen(port, (err) => {
+app.post('/convert', function (req, res) {
+    res.send('This will be the endpoint');
+});
+
+app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
